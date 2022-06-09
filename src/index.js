12672c8357d3fc13520db3f9763eb9eea7133281
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-09 23:46:34 +0600

    React components

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App'
-// import reportWebVitals from './reportWebVitals';
-//
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-//
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 
-//const element = React.createElement('h1',null,'Hello, World!')
-class Clock {
-    print() {
+class Clock extends React.Component{
+    render() {
         return (
             <h1 className='heading'>
-                <span className='text'> Hello World !{new Date().toLocaleTimeString()}</span>
+                <span className='text'> Hello World !{new Date().toLocaleTimeString(this.props.locale)}</span>
             </h1>
         );
     }
 }
 
-const ClockCompoent = new Clock();
-ReactDOM.render(ClockCompoent.print(),document.getElementById('root'))
+
+ReactDOM.render(<Clock locale="bn-BD"/>,document.getElementById('root'))
