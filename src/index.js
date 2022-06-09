7903d6d0da16944fead13af5ccb60332fb284a6b
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-09 21:53:17 +0600

    createElement

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'
-import reportWebVitals from './reportWebVitals';
+// import ReactDOM from 'react-dom/client';
+// import App from './App'
+// import reportWebVitals from './reportWebVitals';
+//
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+//
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const element = React.createElement('h1',null,'Hello, World!')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(element)
