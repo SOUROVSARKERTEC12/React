88284ea6d9866137bd515da26c14ee54c04e80b1
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-13 22:15:18 +0600

    select, textarea, checkbox using react controlled

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,23 +2,59 @@ import React from 'react';
 
 export default class Form extends React.Component {
     state = {
-        title: 'javascript'
+        title: 'javascript',
+        text: 'javascript is not available',
+        library: 'React',
+        isChecked:true,
     };
 
-    handleChange =(e)=>{
-        console.log(e.target.value)
-        this.setState(
-            {title: e.target.value}
-        )
-    }
-    render(){
-        const {title} = this.state
-        return(
+    handleChange = (e) => {
+        if (e.target.type === 'text') {
+            this.setState(
+                { title: e.target.value },
+            );
+        } else if (e.target.type === 'textarea') {
+            this.setState(
+                {
+                    text: e.target.value,
+                },
+            );
+        } else if (e.target.type === 'select-one') {
+            this.setState(
+                {
+                    library: e.target.value,
+                },
+            );
+        }
+        else if (e.target.type === 'checkbox') {
+            this.setState(
+                {
+                    isChecked: e.target.checked,
+                },
+            );
+        }
+    };
+
+    render() {
+        const { title, text, library,isChecked } = this.state;
+        return (
             <div>
                 <form>
-                    <input type="text" placeholder="Enter Title" value={title} onChange={this.handleChange}/>
+                    <input type='text' placeholder='Enter Title' value={title} onChange={this.handleChange} />
+                    <br />
+                    <br />
+                    <textarea name='text' value={text} onChange={this.handleChange}></textarea>
+                    <br />
+                    <br />
+                    <select value={library} onChange={this.handleChange}>
+                        <option value='React'>React</option>
+                        <option value='Angular'>Angular</option>
+                    </select>
+                    <br />
+                    <br />
+                    <input type="checkbox" checked={isChecked} onChange={this.handleChange}/>
                 </form>
             </div>
-        )
+        );
     }
 }
