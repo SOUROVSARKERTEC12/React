3aac011dc40b0ddb55b493c97a4ddaff99cf74d7
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-13 21:23:09 +0600

    Basic Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import ClockList from './components/ClockList';
 import React from 'react';
+import Form from './components/Form';
 
 function App() {
-    const quantities = [1, 2, 3];
+
     return <div>
-        <ClockList quantities={quantities} />
+        <Form />
     </div>;
 }
 
