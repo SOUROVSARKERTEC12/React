8c87b0ac2f6587709f9321e4135e2bd91a03e763
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-14 22:26:13 +0600

    temperature converter mini application using single state

--- src/components/TemperatureInput.js
+++ src/components/TemperatureInput.js
@@ -2,24 +2,13 @@ import React from 'react';
 
 const scaleName = {
     c: 'Celsius',
-    f: 'Fahrenheit'
-}
-export default class TemperatureInput extends React.Component {
-    state = {
-        temperature: '',
-    };
-    onTemperatureChange = (e) => {
-        this.setState({ temperature: e.target.value });
-    };
-
-    render() {
-        const { temperature } = this.state;
-        const {scale} = this.props;
-        return (
-            <fieldset>
-                <legend> Enter temperature in {scaleName[scale]} :</legend>
-                <input type='text' value={temperature} onChange={this.onTemperatureChange} />
-            </fieldset>
-        );
-    }
+    f: 'Fahrenheit',
+};
+export default function TemperatureInput({temperature, scale, onTemperatureChange}) {
+    return (
+        <fieldset>
+            <legend> Enter temperature in {scaleName[scale]} :</legend>
+            <input type='text' value={temperature} onChange={(e) => onTemperatureChange(e, scale)} />
+        </fieldset>
+    );
 }
