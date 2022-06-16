4501fad9ac8287a123db6ae9367e910d819a9858
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-16 22:30:26 +0600

    Order Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import Text from './components/composition/Text';
-import Emoji from './components/composition/Emoji';
-import Bracket from './components/composition/Bracket';
+import ClickCounter from './components/ClickCounter';
+// import Text from './components/composition/Text';
+// import Emoji from './components/composition/Emoji';
+// import Bracket from './components/composition/Bracket';
 //import Text from './components/inheritance/Text';
 //import Calculator from './components/Calculator';
 //import Form from './components/Form';
 
 function App() {
-    return (
-        <Emoji>
-            {({ addEmoji }) => (
-                <Bracket>
-                    {({addBracket}) => <Text addEmoji={addEmoji} addBracket={addBracket} />}
-                </Bracket>)}
-        </Emoji>
-    );
+    return <div className="app"><ClickCounter /></div>
+    // return (
+    //     <Emoji>
+    //         {({ addEmoji }) => (
+    //             <Bracket>
+    //                 {({addBracket}) => <Text addEmoji={addEmoji} addBracket={addBracket} />}
+    //             </Bracket>)}
+    //     </Emoji>
+    // );
     //return <Calculator/>
     // return <div>
     //     {/* <Form /> */}
