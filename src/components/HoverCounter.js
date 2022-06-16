a59a2768223c88b041de59decac65b218076e348
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-16 23:02:56 +0600

    Hover Counter using proops

--- src/components/HoverCounter.js
+++ src/components/HoverCounter.js
@@ -1,20 +1,13 @@
+import withCounter from './HOC/withCounter'
 import React from 'react';
 
-export default class HoverCounter extends React.Component {
-    state = {
-        count : 0,
-    }
-
-    incrementCount = ()=> {
-        this.setState((prevState)=> ({count: prevState.count+1}))
-    }
-
-    render() {
-        const {count} = this.state;
-        return (
-            <div>
-                <h1 onMouseOver={this.incrementCount}>Clicked {count} times</h1>
-            </div>
-        );
-    }
+const HoverCounter =(props)=>{
+    const {count,incrementCount} =props;
+    return (
+        <div>
+            <h1 onMouseOver={incrementCount}>Hovered {count} times</h1>
+        </div>
+    );
 }
+
+export default withCounter(HoverCounter);
