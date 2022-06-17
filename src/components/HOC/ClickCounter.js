c38fe82001ff438d073a09244dbe2a93a94514f3
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-17 21:44:27 +0600

    function render component

--- src/components/HOC/ClickCounter.js
+++ src/components/HOC/ClickCounter.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-export default class ClickCounter extends React.Component {
-
-    render() {
-        const {count, incrementCount} = this.props;
+export default function ClickCounter({count, incrementCount}) {
         return (
             <div>
                 <button type='button' onClick={incrementCount}>Clicked {count} times</button>
             </div>
         );
     }
-};
