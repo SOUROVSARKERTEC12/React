1def234dcf3e67bc1eecd2a62c6ae6b424c29d7a
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-22 23:24:52 +0600

    functional components run successfully

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Todo from './components/TodoClass';
+//import Todo from './components/TodoClass';
+import Todos from './components/Todo';
 
 export default class App extends React.Component {
     render(){
         return (
             <div className="app">
-                <Todo />
+                <Todos />
             </div>
         )
     }
--- src/components/TodoClass.js
+++ src/components/TodoClass.js
@@ -25,7 +25,7 @@ class Todo extends React.Component {
             <div>
                 <p>{todo}</p>
                 <p>
-                    <textarea name="todo" value={todo} onChange={this.handleInput} />
+                    <textarea name='todo' value={todo} onChange={this.handleInput} />
                 </p>
                 <hr />
                 <h2>{warning || 'Good choice!'}</h2>
