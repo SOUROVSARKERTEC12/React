40fc2b022765b12e761f7dd006256839542a9a5a
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-22 23:14:33 +0600

    Todo Component Start Before using Hook

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import React from 'react';
-import ClickCounter from './components/HOC/ClickCounter';
-import Counter from './components/HOC/Counter';
-import Section from './components/HOC/Section';
-import ThemeContext from './components/contexts/themeContext';
-
+import Todo from './components/TodoClass';
 
 export default class App extends React.Component {
-    state = {
-        theme: 'dark',
-        switchTheme: () => {
-            this.setState(({ theme }) => {
-                if (theme === 'dark') {
-                    return {
-                        theme: 'light',
-                    };
-                }
-                return {
-                    theme: 'dark',
-                };
-            });
-        },
-
-    };
-
-    render() {
-
-        return <div className='app'>
-            <Counter>
-                {(counter, incrementCount) => (
-                    <ClickCounter count={counter} incrementCount=
-                        {incrementCount} />
-                )}
-            </Counter>
-            <ThemeContext.Provider value={this.state}>
-                <Section />
-            </ThemeContext.Provider>
-        </div>;
+    render(){
+        return (
+            <div className="app">
+                <Todo />
+            </div>
+        )
     }
 }
 
