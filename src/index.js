89d3228ddd11a2a2379c30d3131620adfa7f7bc2
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-22 23:51:46 +0600

    Roll Back

--- src/index.js
+++ src/index.js
@@ -1,61 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App'
-//import reportWebVitals from './reportWebVitals';
-let states = []; // [0: [ value, setter],1....
-let stateIndex = -1;
+import App from './App'
+import reportWebVitals from './reportWebVitals';
 
-function useState(defaultValue) {
-    const index = ++stateIndex;
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
-    if (states[index]) return states[index];
-
-    const setValue = (newValue) => {
-        states[index][0] = newValue;
-        renderWithSourov();
-    };
-
-    const returnArray = [defaultValue, setValue];
-    states [index] = returnArray;
-    return returnArray;
-}
-
-function App() {
-    const [todo, setTodo] = useState('');
-    const [warning, setWarning] = useState(null);
-
-    const handleInput = (e) => {
-        const inputValue = e.target.value;
-        const updatedWarning = inputValue.includes('.js')
-            ? 'You need JavaScript skill to complete the task. Do you have it?'
-            : null;
-
-        setTodo(inputValue);
-        setWarning(updatedWarning);
-    };
-
-    return (
-        <div>
-            <p>{todo}</p>
-            <p>
-                <textarea name='todo' value={todo} onChange={handleInput} />
-            </p>
-            <hr />
-            <h2>{warning || 'Good choice!'}</h2>
-        </div>
-    );
-}
-
-export default App;
-
-function renderWithSourov() {
-    stateIndex = -1;
-    ReactDOM.render(
-        <App />,
-        document.getElementById('root'),
-    );
-}
-
-renderWithSourov();
-
-//reportWebVitals();
+reportWebVitals();
