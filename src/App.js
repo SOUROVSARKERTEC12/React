4bbccb004a98ebc426279eb57b38a10bba07e885
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-24 23:06:07 +0600

    react.meme() -> higher order component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
-import MyComponent from './components/MyComponent';
+import React,{ useCallback, useMemo, useState } from 'react';
+import Button from './components/Button';
+import ShowCount from './components/ShowCount';
+import Title from './components/Title';
 
+function App() {
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
 
-export default function App() {
-    const [show, setShow] = useState(true);
+    const incrementByOne = useCallback(() => {
+        setCount1((prevCount) => prevCount + 1);
+    }, []);
+
+    const incrementByFive = useCallback(() => {
+        setCount2((prevCount) => prevCount + 5);
+    }, []);
+
 
     return (
-        <div className='app'>
-            <div>{show && <MyComponent />}</div>
-            <p>
-                <button type='button' onClick={() => setShow((preShow) => !preShow)}>
-                    {show ? 'Hide post' : 'Show post'}
-                </button>
-            </p>
+        <div className="app">
+            <Title />
+            <ShowCount count={count1} title="Counter 1" />
+            <Button handleClick={incrementByOne}>Increment by one</Button>
+            <hr />
+            <ShowCount count={count2} title="Counter 2" />
+            <Button handleClick={incrementByFive}>Increment by five</Button>
         </div>
     );
 }
 
-
+export default App;
