1495dcca1f178d1ac44ab917341014336be3a465
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-24 23:24:02 +0600

    useMemo() -> remember the return value of this function

--- src/App.js
+++ src/App.js
@@ -15,11 +15,17 @@ function App() {
         setCount2((prevCount) => prevCount + 5);
     }, []);
 
+    const isEvenOrOdd = useMemo(() => {
+        let i = 0;
+        while (i < 1000000000) i += 1; // costly operation
+        return count1 % 2 === 0;
+    }, [count1]);
 
     return (
         <div className='app'>
             <Title />
             <ShowCount count={count1} title='Counter 1' />
+            <span>{isEvenOrOdd? 'Even' : 'Odd'}</span>
             <Button handleClick={incrementByOne}>Increment by one</Button>
             <hr />
             <ShowCount count={count2} title='Counter 2' />
