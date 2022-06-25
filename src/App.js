a4a12756810248f01a96713fe7f404876d9dcbd0
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-25 22:29:11 +0600

    show from input

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import React, { useCallback, useMemo, useState } from 'react';
-import Button from './components/Button';
-import ShowCount from './components/ShowCount';
-import Title from './components/Title';
+import React from 'react';
+import From from './components/From';
 
-function App() {
-    const [count1, setCount1] = useState(0);
-    const [count2, setCount2] = useState(0);
-
-    const incrementByOne = useCallback(() => {
-        setCount1((prevCount) => prevCount + 1);
-    }, []);
-
-    const incrementByFive = useCallback(() => {
-        setCount2((prevCount) => prevCount + 5);
-    }, []);
-
-    const isEvenOrOdd = useMemo(() => {
-        let i = 0;
-        while (i < 1000000000) i += 1; // costly operation
-        return count1 % 2 === 0;
-    }, [count1]);
-
-    return (
-        <div className='app'>
-            <Title />
-            <ShowCount count={count1} title='Counter 1' />
-            <span>{isEvenOrOdd? 'Even' : 'Odd'}</span>
-            <Button handleClick={incrementByOne}>Increment by one</Button>
-            <hr />
-            <ShowCount count={count2} title='Counter 2' />
-            <Button handleClick={incrementByFive}>Increment by five</Button>
+export default function App(){
+    return(
+        <div className="app">
+            <div>
+                <From/>
+            </div>
         </div>
-    );
+    )
 }
-
-export default App;
---
+++ src/components/From.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function From(){
+    return (
+        <div>
+            <p>
+                <input type='text' placeholder="enter something..." />
+            </p>
+        </div>
+    );
+}
