e1b24adc01335d64ae1645ff283e12306acf3c33
MD.SOUROVSARKAR <36516913+SOUROVSARKERTEC12@users.noreply.github.com>
2022-06-28 08:33:37 +0600

    custom hook first

--- src/components/LayoutComponentTwo.js
+++ src/components/LayoutComponentTwo.js
@@ -1,18 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import useWindowWidth from './Hooks/useWindowWidth';
 
 export default function LayoutComponentTwo() {
-    const [onSmallScreen, setOnSmallScreen] = useState(false);
-
-    const checkScreenSize = () => {
-        setOnSmallScreen(window.innerWidth < 768);
-    };
-
-    useEffect(() => {
-        checkScreenSize();
-        window.addEventListener('resize', checkScreenSize);
-
-        return () => window.removeEventListener('resize', checkScreenSize);
-    }, []);
+    const onSmallScreen = useWindowWidth();
 
     return (
         <div className={onSmallScreen ? 'small' : 'large'}>
